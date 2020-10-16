4cd281748c922b537cdfbaaf5897875a21883630
Michelle Ranly <meranly8@gmail.com>
2020-10-16 01:20:17 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+  attr_accessor :uri, :URL, :response
+
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    @uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+
+  end
+end
